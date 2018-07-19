3f9f85e258b7a5d113866320e3419e5063caf239
Jacob Stelzriede <jstelzri@ucsc.edu>
2018-07-19 10:38:11 -0700

    troubleshooting

--- test.py
+++ test.py
@@ -23,8 +23,8 @@ from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
 # Configuration for the matrix
 options = RGBMatrixOptions()
-options.rows = 64
-options.cols = 64
+#options.rows = 64
+#options.cols = 64
 options.chain_length = 1
 options.parallel = 1
 options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
