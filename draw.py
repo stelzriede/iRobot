8b803f92c4fb69c90039682b4f5c4727fe7e8b34
Jacob Stelzriede <jstelzri@ucsc.edu>
2018-07-22 17:45:09 -0700

    draw script

---
+++ draw.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# (This is an example similar to an example from the Adafruit fork
+#  to show the similarities. Most important difference currently is, that
+#  this library wants RGB mode.)
+#
+# A more complex RGBMatrix example works with the Python Imaging Library,
+# demonstrating a few graphics primitives and image loading.
+# Note that PIL graphics do not have an immediate effect on the display --
+# image is drawn into a separate buffer, which is then copied to the matrix
+# using the SetImage() function (see examples below).
+# Requires rgbmatrix.so present in the same directory.
+
+# PIL Image module (create or load images) is explained here:
+# http://effbot.org/imagingbook/image.htm
+# PIL ImageDraw module (draw shapes to images) explained here:
+# http://effbot.org/imagingbook/imagedraw.htm
+
+from PIL import Image
+from PIL import ImageDraw
+import time
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+# Configuration for the matrix
+options = RGBMatrixOptions()
+options.rows = 64
+options.cols = 64
+options.chain_length = 1
+options.parallel = 1
+options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
+options.led_rgb_sequence = 'RBG'
+
+matrix = RGBMatrix(options = options)
+
+draw.ellipse(6, 10)
